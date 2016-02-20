d71636241c81ab5dce9aaa07ffc59435c874618a
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-02-20 13:39:09 +0100

    Hide images per default

--- example/image.component.ts
+++ example/image.component.ts
@@ -3,12 +3,20 @@ import {LazyLoadImageDirective} from '../src/lazy-load-image.directive';
 
 @Component({
     selector: 'image',
+    styles: [`
+        img {
+            width: 100%;
+            min-height: 1000px;
+            transition: opacity 1s;
+            opacity: 0;
+        }
+
+        img.ng2-lazyloaded {
+            opacity: 1;
+        }
+    `],
     template: `
-        <img
-            src="https://www.placecage.com/1000/1000"
-            [lazyLoad]="image"
-            offset="0"
-        >
+        <img src="https://www.placecage.com/1000/1000" [lazyLoad]="image" offset="0">
     `,
     directives: [LazyLoadImageDirective]
 })
