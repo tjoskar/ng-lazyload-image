33535b2b8a9eaa687b6ae7f5e3b340dfcdb5696a
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-02-20 13:40:05 +0100

    Update order on images so we load a images with color at the to first

--- example/app.component.ts
+++ example/app.component.ts
@@ -11,8 +11,8 @@ import {ImageComponent} from './image.component';
 class AppComponent {
 
     images = [
-        'https://images.unsplash.com/photo-1443890923422-7819ed4101c0?fm=jpg',
         'https://images.unsplash.com/photo-1431887773042-803ed52bed26?fm=jpg',
+        'https://images.unsplash.com/photo-1443890923422-7819ed4101c0?fm=jpg',
         'https://images.unsplash.com/photo-1448960968772-b63b3f40dfc1?fm=jpg',
         'https://images.unsplash.com/photo-1432256851563-20155d0b7a39?fm=jpg'
     ];
