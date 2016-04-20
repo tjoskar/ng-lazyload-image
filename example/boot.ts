0df826130db92cdbfd80266001f708ebc74a97dd
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-04-20 20:53:23 +0200

    :racehorse: Only add parts from rx. We dont need everything

--- example/boot.ts
+++ example/boot.ts
@@ -1,6 +1,14 @@
 import 'es6-shim';
 import 'reflect-metadata';
-import 'rxjs';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/sampleTime';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/take';
 import 'zone.js/dist/zone-microtask';
 import 'zone.js/dist/long-stack-trace-zone';
 import {bootstrap} from 'angular2/platform/browser';
