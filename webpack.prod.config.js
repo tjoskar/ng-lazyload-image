9d49a33f75ee0e92fc71255304f063e6612c673d
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-06-11 14:51:21 +0200

    :hatched_chick: Add a webpack config for production

---
+++ webpack.prod.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
+const devWebpackConfig = require('./webpack.config');
+
+const ENV = process.env.ENV = process.env.NODE_ENV = 'production';
+const HMR = false;
+
+module.exports = Object.assign({}, devWebpackConfig, {
+    devtool: 'source-map',
+    debug: false,
+    cache: false,
+
+    devServer: undefined,
+
+    plugins: [
+        new ForkCheckerPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(true),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8 : true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        }),
+        new webpack.optimize.CommonsChunkPlugin({ name: ['main', 'vendor', 'polyfills'], minChunks: Infinity }),
+        new webpack.DefinePlugin({
+            HMR,
+            ENV: JSON.stringify(ENV)
+        })
+    ]
+};
