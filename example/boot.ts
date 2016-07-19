8d8e405be7e26e9e0cbf506d8afe24493667656f
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-07-19 10:46:32 +0200

    :bug: Do not start the application twice, that is just stupid ;)

--- example/boot.ts
+++ example/boot.ts
@@ -1,8 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
-
 function main() {
     return bootstrap(AppComponent)
     .then(() => '🦄')
