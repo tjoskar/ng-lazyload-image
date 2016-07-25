2095c3985a0494f8bac6485ec1ef225dabaf08f3
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-07-25 12:01:31 +0200

    :sparkles: #13 Use passive scroll option

--- src/scroll-listener.ts
+++ src/scroll-listener.ts
@@ -1,18 +1,31 @@
-import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/sampleTime';
 import 'rxjs/add/operator/share';
+import 'rxjs/add/observable/empty';
 import { Observable } from 'rxjs/Observable';
 
 const scrollListeners = {};
 
 // Only create one scroll listener per target and share the observable.
-// Typical, there will only be one observable
+// Typical, there will only be one observable per application
 export const getScrollListener = (scrollTarget): Observable<any> => {
+    if (!scrollTarget || typeof scrollTarget.addEventListener !== 'function') {
+        console.warn('`addEventListener` on ' + scrollTarget + ' (scrollTarget) is not a function. Skipping this target');
+        return Observable.empty();
+    }
     if (scrollTarget in scrollListeners) {
         return scrollListeners[scrollTarget];
     }
-    scrollListeners[scrollTarget] = Observable.fromEvent(scrollTarget, 'scroll')
+
+    const srollEvent = Observable.create(observer => {
+        const eventName = 'scroll';
+        const handler = event => observer.next(event);
+        const options = { passive: true, capture: false };
+        scrollTarget.addEventListener(eventName, handler, options);
+        return () => scrollTarget.removeEventListener(eventName, handler, options);
+    });
+
+    scrollListeners[scrollTarget] = srollEvent
         .sampleTime(100)
         .share()
         .startWith('');
