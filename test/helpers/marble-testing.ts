fc4ebe26e78e557baf04db5e95ea0302d97b0e4e
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-09-10 16:46:44 +0200

    :art: clean

--- test/helpers/marble-testing.ts
+++ test/helpers/marble-testing.ts
@@ -1,10 +1,10 @@
 declare var global;
 
-export function hot(a: string) {
+export function hot(marbles: string, values?: any) {
     if (!global.rxTestScheduler) {
         throw 'tried to use hot() in async test';
     }
-    return global.rxTestScheduler.createHotObservable(a);
+    return global.rxTestScheduler.createHotObservable(marbles, values);
 }
 
 export function cold() {
