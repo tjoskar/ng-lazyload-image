61478faaeef423d6662307176e623aed24f251c1
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-09-11 10:47:45 +0200

    :green_heart: Run unit test on SauceLabs

--- karma.ci.js
+++ karma.ci.js
@@ -1,14 +1,42 @@
 const karmaDefault = require('./karma.default');
 
 module.exports = config => {
+    // Browsers to run on Sauce Labs
+    const customLaunchers = {
+        'SL_Chrome': {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            version: 'latest'
+        },
+        'SL_Edge': {
+            base: 'SauceLabs',
+            browserName: 'MicrosoftEdge',
+            platform: 'Windows 10',
+            version: 'latest'
+        },
+        'SL_FireFox': {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            version: 'latest'
+        }
+    };
+
     config.set(Object.assign({}, karmaDefault(config), {
         // Webpack please don't spam the console when running in karma!
         webpackServer: { noInfo: true },
 
+        reporters: ['dots', 'saucelabs'],
+
         port: 9876,
-        colors: false,
+        colors: true,
         logLevel: config.LOG_INFO,
-        autoWatch: false,
-        singleRun: true
+        sauceLabs: {
+            testName: 'Angular lazy load images'
+        },
+        captureTimeout: 120000,
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
+        singleRun: true,
+        autoWatch: false
     }));
 };
