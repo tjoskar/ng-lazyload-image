480c16ea00af93ea17bb0f5a73600810930e9a9d
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-09-11 09:03:40 +0200

    :sparkles: Add test-helper as a start up file

---
+++ unit-test.karma.bundle.js
@@ -0,0 +1,7 @@
+// Require all modules ending in ".spec" from the
+// current directory and all subdirectories
+var testContext = require.context('./test', true, /\.test\.ts/);
+
+// For each module, call the context function
+// that will require the file and load it up here.
+testContext.keys().forEach(testContext);
--- wallaby.js
+++ wallaby.js
@@ -4,8 +4,15 @@ const wallabyWebpack = require('wallaby-webpack');
 const webpackPostprocessor = wallabyWebpack({
     entryPatterns: [
         'unit-test.bundle.js',
+        'test/helpers/test-helper.js',
         'test/**/*.test.js'
-    ]
+    ],
+    module: {
+        loaders: [
+            {test: /unit-test\.karma\.bundle\.js/, loader: 'null'}
+        ]
+    },
+    devtool: 'source-map'
 });
 
 module.exports = () => {
@@ -13,10 +20,11 @@ module.exports = () => {
     return {
 
         files: [
-            {pattern: 'test/helpers/test-helper.ts', load: false},
-            {pattern: 'unit-test.bundle.js', load: false},
+            {pattern: 'test/helpers/test-helper.ts', load: false, instrument: false},
+            {pattern: 'unit-test.bundle.js', load: false, instrument: false},
+            {pattern: 'unit-test.karma.bundle.js', load: false, instrument: false},
             {pattern: 'src/**/*.ts', load: false},
-            {pattern: 'test/**/*.ts', load: false},
+            {pattern: 'test/helpers/**/*.ts', load: false, instrument: false},
             {pattern: 'test/**/*.test.ts', ignore: true}
         ],
 
