16c0effcc8e13d783dd88fd8271d40946084c075
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-02-06 18:39:33 +0100

    🎨  CLEAN

--- example/default-image.component.ts
+++ example/default-image.component.ts
@@ -19,20 +19,20 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
         }
     `],
     template: `
-      <img
-        *ngFor="let image of images"
-        src="https://www.placecage.com/1000/1000"
-        [errorImage]="errorImage"
-        [lazyLoad]="image">
+        <img
+            *ngFor="let image of images"
+            src="https://www.placecage.com/1000/1000"
+            [errorImage]="errorImage"
+            [lazyLoad]="image">
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DefaultImageComponent {
-  errorImage = 'https://i.imgur.com/XkU4Ajf.png';
+    errorImage = 'https://i.imgur.com/XkU4Ajf.png';
 
-  images = [
-      'https://images.unsplash.com/photo-1467932760935-519284fc87fa?dpr=2&auto=compress,format&fit=crop&w=1199&h=800&q=80',
-      'https://images.unsplash.com/photo-1468103933896-2c34a78104c2?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80',
-      'https://images.unsplash.com/photo-1471201187657-6406da15e43b?dpr=2&auto=compress,format&fit=crop&w=1199&h=1199&q=80'
-  ];
+    images = [
+        'https://images.unsplash.com/photo-1467932760935-519284fc87fa?dpr=2&auto=compress,format&fit=crop&w=1199&h=800&q=80',
+        'https://images.unsplash.com/photo-1468103933896-2c34a78104c2?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80',
+        'https://images.unsplash.com/photo-1471201187657-6406da15e43b?dpr=2&auto=compress,format&fit=crop&w=1199&h=1199&q=80'
+    ];
 }
