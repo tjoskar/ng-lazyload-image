1fcf7bfe629753d568cb08e8327214c57fb0757b
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-02-06 18:38:20 +0100

    💄  Change the images to smaller ones

--- example/default-image.component.ts
+++ example/default-image.component.ts
@@ -31,8 +31,8 @@ export class DefaultImageComponent {
   errorImage = 'https://i.imgur.com/XkU4Ajf.png';
 
   images = [
-      'https://hd.unsplash.com/photo-1449023859676-22e61b0c0695',
-      'https://hd.unsplash.com/photo-1451324119451-db0ac857463c',
-      'https://hd.unsplash.com/photo-1464746133101-a2c3f88e0dd9'
+      'https://images.unsplash.com/photo-1467932760935-519284fc87fa?dpr=2&auto=compress,format&fit=crop&w=1199&h=800&q=80',
+      'https://images.unsplash.com/photo-1468103933896-2c34a78104c2?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80',
+      'https://images.unsplash.com/photo-1471201187657-6406da15e43b?dpr=2&auto=compress,format&fit=crop&w=1199&h=1199&q=80'
   ];
 }
