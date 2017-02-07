44bed6b4ab097b7b1b8b6e30ac9145b426ac171b
Murhaf Sousli <murhafsousli@gmail.com>
2017-02-07 20:14:48 +0200

    refactor(WindowService)

--- src/lazyload-image.ts
+++ src/lazyload-image.ts
@@ -5,20 +5,19 @@ import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
 import { Observable } from 'rxjs/Observable';
-import { getScrollListener } from './scroll-listener';
 
-function isVisible(element: HTMLElement, threshold = 0, _window = window) {
+function isVisible(element: HTMLElement, threshold = 0, window) {
     const rect = element.getBoundingClientRect();
     // Is the element in viewport but larger then viewport itself
     const elementLargerThenViewport = rect.top <= threshold && rect.bottom >= -threshold;
     // Is the top of the element in the viewport
-    const topInsideViewport = rect.top >= 0 && rect.top <= _window.innerHeight;
+    const topInsideViewport = rect.top >= 0 && rect.top <= window.innerHeight;
     // Is the bottom of the element in the viewport
-    const belowInsideViewport = rect.bottom >= 0 && rect.bottom <= _window.innerHeight;
+    const belowInsideViewport = rect.bottom >= 0 && rect.bottom <= window.innerHeight;
     // Is the right side of the element in the viewport
-    const rightsideInViewport = rect.right >= -threshold && (rect.right - threshold) <= _window.innerWidth;
+    const rightsideInViewport = rect.right >= -threshold && (rect.right - threshold) <= window.innerWidth;
     // Is the left side of the element is the viewport
-    const leftsideInViewport = rect.left >= -threshold && (rect.left - threshold) <= _window.innerWidth;
+    const leftsideInViewport = rect.left >= -threshold && (rect.left - threshold) <= window.innerWidth;
 
     return (
         elementLargerThenViewport ||
@@ -62,10 +61,10 @@ function setLoadedStyle(element: HTMLElement) {
     return element;
 }
 
-export function lazyLoadImage(image: HTMLElement, imagePath: string, errorImgPath: string, offset: number) {
+export function lazyLoadImage(image: HTMLElement, imagePath: string, errorImgPath: string, offset: number, window) {
     return (scrollObservable: Observable<Event>) => {
         return scrollObservable
-            .filter(() => isVisible(image, offset))
+            .filter(() => isVisible(image, offset, window))
             .take(1)
             .mergeMap(() => loadImage(imagePath))
             .do(() => setImage(image, imagePath))
