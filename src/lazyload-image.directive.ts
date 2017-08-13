b699946ff79e8b36846562d192af36492178c71b
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-08-13 20:13:50 +0200

    ✨  Re subscribe every time the props changes

--- src/lazyload-image.directive.ts
+++ src/lazyload-image.directive.ts
@@ -1,9 +1,22 @@
 import 'rxjs/add/operator/let';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Directive, ElementRef, EventEmitter, Input, NgZone, Output } from '@angular/core';
 import { getScrollListener } from './scroll-listener';
 import { lazyLoadImage } from './lazyload-image';
 
 const target = typeof window !== 'undefined' ? window : undefined;
+
+interface LazyLoadImageDirectiveProps {
+    lazyImage: string;
+    defaultImage: string;
+    errorImage: string;
+    scrollTarget: Object;
+    scrollObservable: Observable<Event>;
+    offset: number;
+}
 
 @Directive({
     selector: '[lazyLoad]'
@@ -16,18 +29,28 @@ export class LazyLoadImageDirective {
     @Input() scrollObservable;      // Pass your own scroll emitter
     @Input() offset: number;        // The number of px a image should be loaded before it is in view port
     @Output() onLoad: EventEmitter<boolean> = new EventEmitter(); // Callback when an image is loaded
-    elementRef: ElementRef;
-    ngZone: NgZone;
-    platformId: string;
-    scrollSubscription;
+    private propertyChanges$: ReplaySubject<LazyLoadImageDirectiveProps>;
+    private elementRef: ElementRef;
+    private ngZone: NgZone;
+    private platformId: string;
+    private scrollSubscription;
 
-  constructor(
-    el: ElementRef,
-    ngZone: NgZone,
-  ) {
-    this.elementRef = el;
-    this.ngZone = ngZone;
-  }
+    constructor(el: ElementRef, ngZone: NgZone) {
+        this.elementRef = el;
+        this.ngZone = ngZone;
+        this.propertyChanges$ = new ReplaySubject();
+    }
+
+    ngOnChanges() {
+        this.propertyChanges$.next({
+            lazyImage: this.lazyImage,
+            defaultImage: this.defaultImage,
+            errorImage: this.errorImage,
+            scrollTarget: this.scrollTarget,
+            scrollObservable: this.scrollObservable,
+            offset: this.offset,
+        });
+    }
 
     ngAfterContentInit() {
         /**
@@ -40,20 +63,24 @@ export class LazyLoadImageDirective {
         }
 
         this.ngZone.runOutsideAngular(() => {
+            let scrollObservable: Observable<Event>;
             if (this.scrollObservable) {
-                this.scrollSubscription = this.scrollObservable
-                    .startWith('')
-                    .let(lazyLoadImage(this.elementRef.nativeElement, this.lazyImage, this.defaultImage, this.errorImage, this.offset))
-                    .subscribe(success => {
-                        this.onLoad.emit(success);
-                    });
+                scrollObservable = this.scrollObservable.startWith('');
             } else {
-                this.scrollSubscription = getScrollListener(this.scrollTarget)
-                    .let(lazyLoadImage(this.elementRef.nativeElement, this.lazyImage, this.defaultImage, this.errorImage, this.offset))
-                    .subscribe(success => {
-                        this.onLoad.emit(success);
-                    });
+                scrollObservable = getScrollListener(this.scrollTarget);
             }
+            this.scrollSubscription = this.propertyChanges$
+                .debounceTime(10)
+                .switchMap(props => scrollObservable.let(
+                    lazyLoadImage(
+                        this.elementRef.nativeElement,
+                        props.lazyImage,
+                        props.defaultImage,
+                        props.errorImage,
+                        props.offset
+                    )
+                ))
+                .subscribe(success => this.onLoad.emit(success));
         });
     }
 
