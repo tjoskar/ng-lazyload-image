ca0a1c1f5d12f74ab153bb2c35e2596fc6292e3c
rimlin <ilmirw@gmail.com>
2018-01-09 22:34:48 +0300

    Extract all css class names to object

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export function isWindowExists() {
     return typeof window !== 'undefined';
 }
+
+export const cssClassNames = {
+    loaded: 'ng-lazyloaded',
+    loading: 'ng-lazyloading',
+    failed: 'ng-failed-lazyloaded',
+}
