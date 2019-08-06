bf2105f0363b01860799e3157333e8349750a7a8
tjoskar <kontakta@oskarkarlsson.nu>
2019-08-06 15:46:25 +0200

    ⚡️  decode image before render

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,41 +1,45 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    styles: [`
-        .menu {
-            display: flex;
-            width: 50%;
-            justify-content: space-between;
-            margin: 20px auto;
-        }
+  selector: 'my-app',
+  styles: [
+    `
+      .menu {
+        display: flex;
+        width: 50%;
+        justify-content: space-between;
+        margin: 20px auto;
+      }
 
-        a {
-          text-decoration: none;
-          font-size: 16px;
-          padding: 10px;
-          transition: all .2s ease-out;
-        }
+      a {
+        text-decoration: none;
+        font-size: 16px;
+        padding: 10px;
+        transition: all 0.2s ease-out;
+      }
 
-        a.active, a:hover {
-          background-color: #3498db;
-          color: #fff !important;
-        }
-    `],
-    template: `
-        <div class="menu">
-            <a routerLink="/fade-in-image" routerLinkActive="active">Fade in image</a>
-            <a routerLink="/default-image" routerLinkActive="active">Default image</a>
-            <a routerLink="/pixelated-image" routerLinkActive="active">Pixelated transform</a>
-            <a routerLink="/bg-image" routerLinkActive="active">Background image</a>
-            <a routerLink="/scroll-container" routerLinkActive="active">Scroll Container</a>
-            <a routerLink="/on-load" routerLinkActive="active">OnLoad event</a>
-            <a routerLink="/change-image" routerLinkActive="active">Change image</a>
-            <a routerLink="/responsive-image" routerLinkActive="active">Responsive image</a>
-        </div>
+      a.active,
+      a:hover {
+        background-color: #3498db;
+        color: #fff !important;
+      }
+    `
+  ],
+  template: `
+    <div class="menu">
+      <a routerLink="/fade-in-image" routerLinkActive="active">Fade in image</a>
+      <a routerLink="/default-image" routerLinkActive="active">Default image</a>
+      <a routerLink="/pixelated-image" routerLinkActive="active">Pixelated transform</a>
+      <a routerLink="/bg-image" routerLinkActive="active">Background image</a>
+      <a routerLink="/scroll-container" routerLinkActive="active">Scroll Container</a>
+      <a routerLink="/on-load" routerLinkActive="active">OnLoad event</a>
+      <a routerLink="/change-image" routerLinkActive="active">Change image</a>
+      <a routerLink="/responsive-image" routerLinkActive="active">Responsive image</a>
+      <a routerLink="/big-images" routerLinkActive="active">Big images</a>
+    </div>
 
-        <router-outlet></router-outlet>
-    `,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    <router-outlet></router-outlet>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {}
--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule }   from '@angular/router';
-import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { RouterModule } from '@angular/router';
+import { LazyLoadImageModule, intersectionObserverPreset } from 'ng-lazyload-image';
 import { AppComponent } from './app.component';
 import { FadeInImageComponent } from './pages/fade-in-image.component';
 import { BgImageComponent } from './pages/bg-image.component';
@@ -11,38 +11,41 @@ import { ScrollContainerComponent } from './pages/scroll-container.component';
 import { ChangeImageComponent } from './pages/changing-image.component';
 import { OnLoadComponent } from './pages/onload.component';
 import { ResponsiveImageComponent } from './pages/responsive-image.component';
+import { BigImagesComponent } from './pages/big-images.component';
 
 @NgModule({
-    declarations: [
-      AppComponent,
-      FadeInImageComponent,
-      BgImageComponent,
-      DefaultImageComponent,
-      PixelatedImageComponent,
-      ScrollContainerComponent,
-      ChangeImageComponent,
-      OnLoadComponent,
-      ResponsiveImageComponent,
-    ],
-    imports: [
-      BrowserModule,
-      RouterModule.forRoot([
-        {
-          path: '',
-          redirectTo: '/fade-in-image',
-          pathMatch: 'full'
-        },
-        { path: 'change-image', component: ChangeImageComponent },
-        { path: 'fade-in-image', component: FadeInImageComponent },
-        { path: 'bg-image', component: BgImageComponent },
-        { path: 'default-image', component: DefaultImageComponent },
-        { path: 'pixelated-image', component: PixelatedImageComponent },
-        { path: 'scroll-container', component: ScrollContainerComponent },
-        { path: 'on-load', component: OnLoadComponent },
-        { path: 'responsive-image', component: ResponsiveImageComponent },
-      ]),
-      LazyLoadImageModule
-    ],
-    bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent,
+    FadeInImageComponent,
+    BgImageComponent,
+    DefaultImageComponent,
+    PixelatedImageComponent,
+    ScrollContainerComponent,
+    ChangeImageComponent,
+    OnLoadComponent,
+    ResponsiveImageComponent,
+    BigImagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/fade-in-image',
+        pathMatch: 'full'
+      },
+      { path: 'change-image', component: ChangeImageComponent },
+      { path: 'fade-in-image', component: FadeInImageComponent },
+      { path: 'bg-image', component: BgImageComponent },
+      { path: 'default-image', component: DefaultImageComponent },
+      { path: 'pixelated-image', component: PixelatedImageComponent },
+      { path: 'scroll-container', component: ScrollContainerComponent },
+      { path: 'on-load', component: OnLoadComponent },
+      { path: 'responsive-image', component: ResponsiveImageComponent },
+      { path: 'big-images', component: BigImagesComponent }
+    ]),
+    LazyLoadImageModule.forRoot(intersectionObserverPreset)
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
