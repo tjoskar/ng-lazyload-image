015083308afde1610d9f7755732ab992f350a579
tjoskar <kontakta@oskarkarlsson.nu>
2020-11-14 09:34:22 +0100

    ✨  Make directive public
    Fixes #486

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-export { LazyLoadImageModule } from './src/lazyload-image.module';
 export { IntersectionObserverHooks } from './src/intersection-observer-hooks/hooks';
+export { LazyLoadImageDirective } from './src/lazyload-image.directive';
+export { LazyLoadImageModule } from './src/lazyload-image.module';
 export { ScrollHooks } from './src/scroll-hooks/hooks';
 export { SharedHooks } from './src/shared-hooks/hooks';
-export { Attributes, StateChange, Hooks } from './src/types';
 export { LAZYLOAD_IMAGE_HOOKS } from './src/token';
+export { Attributes, Hooks, StateChange } from './src/types';
